2f6d0051e74d11f97ae6ef3f9e0b92ef081aeeb7
yocolink <lu.nitoumbi@gmail.com>
2021-10-15 17:11:21 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <FlipCard />
+</template>
+
+<script>
+import FlipCard from "./components/FlipCard.vue";
+export default {
+  name: "App",
+  components: {
+    FlipCard: FlipCard,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/FlipCard.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>flipcard</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
